6e27e5df7a803b414d45ce2e68cfe8a5d2b40829
Robin_auxietre <robin.auxietre@epitech.eu>
2021-02-02 19:03:51 +0100

    patch elou branch

--- day_4/my_swap.c
+++ day_4/my_swap.c
@@ -2,18 +2,18 @@
 
 void my_swap(int *a, int *b)
 {
-    int *c = 0;
+    int c;
 
-    *c = *a;
+    c = *a;
     *a = *b;
-    *b = *c;    
-    printf ("%d%d", *a,*b);
+    *b = c;
+    printf ("%d %d", a[0], b[0]);
 }
 
 int main(void)
 {
-    int *a = 2;
-    int *b = 9;
-    my_swap(&a, &b);
+    int a[3] = {2, 3, 4};
+    int b[3] = {9, 46, 34};
+    my_swap(a, b);
     return 0;
 }
