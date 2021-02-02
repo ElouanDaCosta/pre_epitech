41f75efe673ffb6969730851e3e617bb32638a82
Elouan <elouandacostapeixoto@gmail.com>
2021-02-02 18:20:21 +0100

    prout

--- day_4/my_swap.c
+++ day_4/my_swap.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
 void my_swap(int *a, int *b)
+{
+    int *c = 0;
+
+    *c = *a;
+    *a = *b;
+    *b = *c;    
+    printf ("%d%d", *a,*b);
+}
+
+int main(void)
+{
+    int *a = 2;
+    int *b = 9;
+    my_swap(&a, &b);
+    return 0;
+}
