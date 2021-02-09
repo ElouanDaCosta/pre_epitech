09d419a1e0c73d0a30ebea2e0d98d77af1930522
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2021-02-09 20:55:10 +0100

    Push my_strlen.c
    J'ai ni réussi a push avec git et mon code marche pas mais tkt jtm

--- day_4/my_strlen.c
+++ day_4/my_strlen.c
@@ -1,5 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
 int my_strlen(char const *str)
 {
-    
+    int i = 0;
+    for(; str[i] != '\0'; i++);
+    return i;
+}
 
+int main(void)
+{
+    my_strlen("gros caca"); 
+    return 0;
 }
