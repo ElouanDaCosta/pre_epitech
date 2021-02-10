0b5f8721ddf4591cbbfe13c478738119cd88f2b2
roxietre <56362287+roxietre@users.noreply.github.com>
2021-02-10 13:46:43 +0100

    Update my_strlen.c

--- day_4/my_strlen.c
+++ day_4/my_strlen.c
@@ -11,6 +11,6 @@ int my_strlen(char const *str)
 int main(void)
 {
     int i = my_strlen("gros caca"); 
-    printf("%d", i);
+    printf("%d", i);// printf("%d, my_strlen("hello world"));
     return 0;
 }
